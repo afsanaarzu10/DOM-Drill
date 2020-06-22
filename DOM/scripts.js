0545338ea7ad364d2a26942e104a7317fdc8f049
Arzash <arzash@Anikas-MacBook-Air.local>
2020-06-22 21:56:15 +0600

    First attempt

---
+++ DOM/scripts.js
@@ -0,0 +1,56 @@
+var div1 = document.createElement('div');
+div1.setAttribute('class','header-container');
+
+var Head1 =document.createElement('h1');
+Head1.setAttribute('class', 'h1');
+var Text1 = document.createTextNode('This is an h1');
+Head1.appendChild(Text1);
+div1.appendChild(Head1);
+
+
+window.addEventListener('DOMContentLoaded', function () {
+    document.body.appendChild(div1); // add the header to the document body
+});
+
+
+var Head2 = document.createElement('h2');
+var Text2 = document.createTextNode('This is an h2');
+Head2.setAttribute('class', 'h2');
+Head2.appendChild(Text2);
+div1.appendChild(Head2);
+
+var Head3 = document.createElement('h3');
+var Text3 = document.createTextNode('This is an h3');
+Head3.setAttribute('class', 'h3');
+Head3.appendChild(Text3);
+div1.appendChild(Head3);
+
+var Head4 = document.createElement('h4');
+var Text4 = document.createTextNode('This is an h4');
+Head4.setAttribute('class', 'h4');
+Head4.appendChild(Text4);
+div1.appendChild(Head4);
+
+var Head5 = document.createElement('h5');
+var Text5 = document.createTextNode('This is an h5');
+Head5.setAttribute('class', 'h5');
+Head5.appendChild(Text5);
+div1.appendChild(Head5);
+
+var Head6 = document.createElement('h6');
+var Text6 = document.createTextNode('This is an h6');
+Head6.setAttribute('class', 'h6');
+Head6.appendChild(Text6);
+div1.appendChild(Head6);
+
+
+
+Head1.addEventListener("click", myFunction);
+
+function myFunction() {
+    
+    var list = ['blue','red','yellow','black','brown','green','orange','purple'];
+    var colorcontainer = list[Math.floor(Math.random() * 10)];
+    Head1.style.color = colorcontainer;
+}
+
